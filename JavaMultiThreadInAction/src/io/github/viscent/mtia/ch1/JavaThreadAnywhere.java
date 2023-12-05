63fd52b17e03aa3834ab9ada86ad6a7f37f77201
MaxQiu <yueyiqiu178@gmail.com>
2023-12-05 14:45:39 +0800

    加上換行符號

--- JavaMultiThreadInAction/src/io/github/viscent/mtia/ch1/JavaThreadAnywhere.java
+++ JavaMultiThreadInAction/src/io/github/viscent/mtia/ch1/JavaThreadAnywhere.java
@@ -21,7 +21,7 @@ public class JavaThreadAnywhere {
     // 获取当前线程的线程名称
     String currentThreadName = currentThread.getName();
 
-    System.out.printf("The main method was executed by thread:%s",
+    System.out.printf("The main method was executed by thread:%s%n",
         currentThreadName);
     Helper helper = new Helper("Java Thread AnyWhere");
     helper.run();
@@ -41,9 +41,9 @@ public class JavaThreadAnywhere {
       // 获取当前线程的线程名称
       String currentThreadName = currentThread.getName();
 
-      System.out.printf("The doSomething method was executed by thread:%s",
+      System.out.printf("The doSomething method was executed by thread:%s%n",
           currentThreadName);
-      System.out.println("Do something with " + message);
+      System.out.println("Do something with " + message + "%n");
     }
 
     @Override
