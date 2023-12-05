b791a03c1b2fc750d0ee08340d758c5d14b61f1b
MaxQiu <yueyiqiu178@gmail.com>
2023-12-05 14:53:18 +0800

    補上AtomicityExample

---
+++ JavaMultiThreadInAction/src/io/github/viscent/mtia/ch2/AtomicityExample.java
@@ -0,0 +1,49 @@
+package io.github.viscent.mtia.ch2;
+
+public class AtomicityExample {
+
+    private HostInfo hostInfo;
+
+    public void updateHostInfo(String ip, int port) {
+        hostInfo.setIp(ip);
+        hostInfo.setPort(port);
+    }
+
+    public void updateHostInfo2(String ip, int port) {
+        HostInfo hostInfo = new HostInfo();
+        hostInfo.setIp(ip);
+        hostInfo.setPort(port);
+    }
+
+    public void connectToHost() {
+        String ip = hostInfo.getIp();
+        int port = hostInfo.getPort();
+        connectToHost(ip, port);
+    }
+
+    private void connectToHost(String ip, int port) {
+
+    }
+
+    public static class HostInfo {
+        private String ip;
+
+        private int port;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+    }
+}
